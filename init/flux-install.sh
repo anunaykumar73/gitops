1db2716f410ceda89866d7012d140662d6868b9d
Anunay Kumar <anunay_kumar@epam.com>
2021-08-04 03:09:54 +0530

    test flux

--- init/flux-install.sh
+++ init/flux-install.sh
@@ -3,5 +3,6 @@ fluxctl install \
 --git-user=${GHUSER} \
 --git-email=${GHUSER}@gmail.com \
 --git-url=git@github.com:${GHUSER}/gitops \
+--git-branch=main \
 --git-path=clusters/docker-desktop \
 --namespace=flux | kubectl apply -f -
